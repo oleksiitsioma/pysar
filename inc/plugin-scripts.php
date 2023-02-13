02d0e336e8bf7d6e34b31bca85d1f83cdd574bcc
oleksiitsioma <oleksiitsioma@gmail.com>
2023-02-13 05:15:43 -0800

    Fixed filepaths issues

--- inc/plugin-scripts.php
+++ inc/plugin-scripts.php
@@ -2,5 +2,5 @@
 
 
 add_action( 'admin_enqueue_scripts', function(){
-	wp_enqueue_style( 'pysar-admin', plugin_dir_url( __FILE__ ) .'build/admin-style.css' );
+	wp_enqueue_style( 'pysar-admin', plugin_dir_url( __DIR__ ) .'build/admin-style.css' );
 }, 99 );
